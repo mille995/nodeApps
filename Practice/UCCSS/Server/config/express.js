b1ae58f63719d4fa3a65476701608ff2d5f15440
Joyce Miller <mille995@uwm.edu>
2018-10-22 09:53:47 -0500

    Added Logger

--- Practice/UCCSS/Server/config/express.js
+++ Practice/UCCSS/Server/config/express.js
@@ -1,22 +1,28 @@
 var express = require('express');
+var morgan = require('morgan');
+var logger = require('./logger');
 
 module.exports = function (app, config) {
 
-  app.use(function (req, res, next) {
-    console.log('Request from ' + req.connection.remoteAddress);
+    app.use(function (req, res, next) {
+    logger.log('info','Request from ' + req.connection.remoteAddress);
+    console.log("port: ", config.port)
     next();
   });  
+
+  app.use(morgan('dev'));
 
   app.use(express.static(config.root + '/public'));
 
   app.use(function (req, res) {
+    logger.log('error', 'File not found');
     res.type('text/plan');
     res.status(404);
     res.send('404 Not Found');
   });
 
   app.use(function (err, req, res, next) {
-    console.error(err.stack);
+    console.error(err.stack);  // writes an error object to the console.  Middleware will pass that error, e.g., from DB
     res.type('text/plan');
     res.status(500);
     res.send('500 Sever Error');
