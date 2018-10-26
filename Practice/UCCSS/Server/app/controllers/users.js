6204ad32f914310e55e98815807b878ec8a9ca21
Joyce Miller <mille995@uwm.edu>
2018-10-25 22:56:07 -0500

    updated with asyncHandler

--- Practice/UCCSS/Server/app/controllers/users.js
+++ Practice/UCCSS/Server/app/controllers/users.js
@@ -1,37 +1,60 @@
 console.log("controllers/users.js");
 
+
+
 var express = require('express'),
     router = express.Router(),
     logger = require('../../config/logger'),
     mongoose = require('mongoose'),
     User = mongoose.model('User'); // uppercase is the model (vs. the instance)
+asyncHandler = require('express-async-handler');
 
 module.exports = function (app, config) {
     app.use('/api', router);
     // all routes must be in this area
 
-    router.route('/users').get(function (req, res, next) {
-        logger.log('info', 'Get all users');
-        res.status(200).json({ message: 'Get all users' });
-
-    });
-
-    router.route('/users/:id').get(function (req, res, next) {
-        logger.log('info', 'Get user %s', req.params.id);
-        res.status(200).json({ message: 'Get user ' + req.params.id });
-    });
-
-    router.route('/users').post(function (req, res, next) {
-        logger.log('Create User', 'verbose');
+    router.post('/users', asyncHandler(async (req, res) => {
+        logger.log('info', 'Creating user');
         var user = new User(req.body);
-        user.save()
+        await user.save()
             .then(result => {
                 res.status(201).json(result);
             })
-            .catch( err => {
-                return next(err);
-            });
-    })
+    }));
+
+    router.get('/users/:id', asyncHandler(async (req, res) => {
+        logger.log('info', 'Get user %s', req.params.id);
+        await User.findById(req.params.id).then(result => {
+            res.status(200).json(result);
+        })
+    }));
+
+    router.get('/users', asyncHandler(async (req, res) => {
+        logger.log('info', 'Get all users');
+        let query = User.find();
+        query.sort(req.query.order)
+        await query.exec().then(result => {
+            res.status(200).json(result);
+        })
+    }));
+
+
+    router.put('/users', asyncHandler(async (req, res) => {
+        logger.log('info', 'Updating user');
+        await User.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true })
+            .then(result => {
+                res.status(200).json(result);
+            })
+    }));
+
+    router.delete('/users/:id', asyncHandler(async (req, res) => {
+        logger.log('info', 'Deleting user %s', req.params.id);
+        await User.remove({ _id: req.params.id })
+            .then(result => {
+                res.status(200).json(result);
+            })
+    }));
+
 
     // var obj = { 'email': email, 'password': password };
     // res.status(201).json(obj);
