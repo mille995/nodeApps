718cb7ccb24bbd54cb2076ea27bf4d558eed2c61
Joyce Miller <mille995@uwm.edu>
2018-10-28 17:36:33 -0500

    server with simple routes

---
+++ Practice/PROJ2/Server/index.js
@@ -0,0 +1,20 @@
+var http = require('http');
+http.createServer(function(req, res){
+	var path = req.url;
+	console.log(path);
+	switch(path){
+		case '/':
+			 res.writeHead(200, {"Content-Type": "text/plain"});
+			 res.end('Homepage');
+		case '/about':
+			res.writeHead(200, {"Content-Type": "text/plain"});
+			res.end('About');
+		default:
+			res.writeHead(404, {"Content-Type": "text/plain"});
+			res.end('Not Found');
+
+	}
+}).listen(1337);
+
+console.log('Server running at http://127.0.0.1:1337/');
+
