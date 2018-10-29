e2185b30af7628e95fae83f519eac2e6a0b31b2d
Joyce Miller <mille995@uwm.edu>
2018-10-28 21:09:40 -0500

    logger functionality added

--- Practice/UCCSSNEW/Server/config/express.js
+++ Practice/UCCSSNEW/Server/config/express.js
@@ -1,12 +1,14 @@
 
 var express = require('express');
+var morgan = require('morgan'); // used for logging
+var logger = require('./logger');  // in the same folder (config)
 
 module.exports = function (app, config) {
     // app is the express object from express.js
     // config is the config option we import from the config file
 
   app.use(function (req, res, next) {
-    console.log('Request from ' + req.connection.remoteAddress);
+    logger.log('info','Request from ' + req.connection.remoteAddress);
     next();
   });  
 
@@ -17,13 +19,14 @@ module.exports = function (app, config) {
   // ********* start error handling
 
   app.use(function (req, res) {
+    logger.log('error','File not found');
     res.type('text/plan');
     res.status(404);
     res.send('404 Not Found');
   });
 
   app.use(function (err, req, res, next) {
-    console.error(err.stack);
+    console.error(err.stack);  // leave this as console because it will write the error object to the console
     res.type('text/plan');
     res.status(500);
     res.send('500 Sever Error');
@@ -31,6 +34,6 @@ module.exports = function (app, config) {
 
     // ********* end error handling
 
-  console.log("Starting application");
+  logger.log('info','Starting application');
 
 };
