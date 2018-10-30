6dedb0f6172cda9274222cdf78012d1c2e5d2b64
Joyce Miller <mille995@uwm.edu>
2018-10-29 22:02:34 -0500

    delete

--- PracticeMidterm/Server/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var config = require('./config/config');
-var app = express();
-var port = process.env.port || 3300
-require('./config/express')(app, config);
-require('http').createServer(app).listen(config.port, function () {
-console.log("HTTP Server listening on port: %d, in %s mode", config.port, app.get('env'));
-});
