186a58191ccfbbef651fdbdfe349d24167474dd2
Joyce Miller <mille995@uwm.edu>
2018-11-04 20:20:08 -0600

    added mongoose, bluebird, routes, glob, async handler

--- Practice/REVIEW/Server/config/config.js
+++ Practice/REVIEW/Server/config/config.js
@@ -7,18 +7,22 @@ var config = {
         root: rootPath,
         app: { name: 'UCCSS' },
         port: 5000,
+        db: 'mongodb://127.0.0.1/helpMe-dev'
     },
 
     test: {
         root: rootPath,
         app: { name: 'UCCSS' },
         port: 4000,
+        db: 'mongodb://127.0.0.1/helpMe-test'
     },
-    
+
     production: {
         root: rootPath,
         app: { name: 'UCCSS' },
         port: 80,
+        db: 'mongodb://127.0.0.1/helpMe'
+
     }
 };
 
--- Practice/REVIEW/Server/config/express.js
+++ Practice/REVIEW/Server/config/express.js
@@ -2,12 +2,37 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
 var logger = require('./logger');
+var mongoose = require('mongoose'); 
+var bluebird = require('bluebird');
+var glob = require('glob');
+asyncHandler = require('express-async-handler');
+
 
 
 module.exports = function (app, config) {
 
-    if (process.env.NODE_ENV !== 'test') {   // added if statement for testing
+    // *** show the database is running
+    logger.log('info', "Loading Mongoose functionality");
+    mongoose.Promise = bluebird;
+    mongoose.connect(config.db);
+    var db = mongoose.connection;
+    db.on('error', function () {
+        throw new Error('unable to connect to database at ' + config.db);
+    });
+
+
+    // *** end show the database is running
+
+    if (process.env.NODE_ENV !== 'test') {   // added 'if' statement for testing; dont write to console for test
         app.use(morgan('dev'));
+
+        // *** added for database connection
+        mongoose.set('debug', true);
+        mongoose.connection.once('open', function callback() {
+            logger.log('info', 'Mongoose connected to the database');
+        });
+
+        // standard
         app.use(function (req, res, next) {
             logger.log('info', 'Request from ' + req.connection.remoteAddress);
             next();
@@ -16,6 +41,22 @@ module.exports = function (app, config) {
 
     // for static files
     app.use(express.static(config.root + '/public'));
+
+    // // require models and controllers -- only used without glob
+    // require('../app/models/users');
+    // require('../app/controllers/users')(app, config);
+
+    // use glob for models and controllers
+    var models = glob.sync(config.root + '/app/models/*.js');
+    models.forEach(function (model) {
+        require(model);
+    });
+
+    var controllers = glob.sync(config.root + '/app/controllers/*.js');
+    controllers.forEach(function (controller) {
+        require(controller)(app, config);
+    });
+
 
     // *** unit test data for unit testing - create an array of users and return the array to api/users
     var users = [{ name: 'John', email: 'woo@hoo.com' },
