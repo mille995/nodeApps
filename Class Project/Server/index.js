d3bd55eef1a412a3373f0a51dbe790cb6f288524
Joyce Miller <mille995@uwm.edu>
2018-11-18 15:43:02 -0600

    restart

--- Class Project/Server/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var config = require('./config/config');
-var app = express();
-
-require('./config/express')(app, config);
-
-require('http').createServer(app).listen(port, function () {
-    console.log("HTTP Server listening on port: %d, in %s mode", config.port, app.get('env'));
-});
