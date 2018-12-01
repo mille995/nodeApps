0bb83bfa688e83d23cc33e389180eb9abc25044e
Joyce Miller <mille995@uwm.edu>
2018-11-30 21:54:41 -0600

    add bcrypt in user model and controller

--- Class Project/UCCSS/Server/app/models/users.js
+++ Class Project/UCCSS/Server/app/models/users.js
@@ -2,15 +2,55 @@
 
 var Mongoose = require('mongoose');
 var Schema = Mongoose.Schema;
+Bcrypt = require('bcryptjs');
 
+// note:  there are both UserSchema and userSchema variables in the instructions
+// all code has been changed to use userSchema (lower case u)
 var userSchema = new Schema({
-    firstname: { type: String, require: true },
-    lastname: { type: String, require: true},
-    active: {type: Boolean, require: true, default: true},
-    role: {type: String, require: true, enum: ['admin', 'user', 'staff']},
-    email: {type: String, require: true, unique: true},
-    password: {type: String, require: true},
-    dateRegistered: { type: Date, default: Date.now }
+  firstname: { type: String, require: true },
+  lastname: { type: String, require: true },
+  active: { type: Boolean, require: true, default: true },
+  role: { type: String, require: true, enum: ['admin', 'user', 'staff'] },
+  email: { type: String, require: true, unique: true },
+  password: { type: String, require: true },
+  dateRegistered: { type: Date, default: Date.now }
+});
+
+// userSchema.pre - pre hook needs to go after the definition and before the model
+// this will encrypt the password
+userSchema.pre('save', function (next) {
+  var person = this;
+  if (this.isModified('password') || this.isNew) {
+    Bcrypt.genSalt(10, function (err, salt) {
+      if (err) {
+        return next(err);
+      }
+      Bcrypt.hash(person.password, salt, function (err, hash) {
+        if (err) {
+          return next(err);
+        }
+        person.password = hash;
+        next();
+      });
+    });
+  } else {
+    return next();
+  }
+});
+
+// this will compare the password that is sent in with the stored password
+userSchema.methods.comparePassword = function (passw, cb) {
+  Bcrypt.compare(passw, this.password, function (err, isMatch) {
+      if (err) {
+          return cb(err);
+      }
+      cb(null, isMatch);
   });
-  
-  module.exports = Mongoose.model('User', userSchema);
+};
+
+userSchema.virtual('fullName')
+.get(function(){
+  return this.firstname + ' ' + this.lastname;
+});
+
+module.exports = Mongoose.model('User', userSchema);
