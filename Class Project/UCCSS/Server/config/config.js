8fbd7954425c62ae2f28160ea4a637046da4a445
Joyce Miller <mille995@uwm.edu>
2018-12-07 22:08:19 -0600

    Revert "config.js add uploads"
    This reverts commit 7c3043961b87fbf8d27e19941ef093946bf23769.

--- Class Project/UCCSS/Server/config/config.js
+++ Class Project/UCCSS/Server/config/config.js
@@ -8,8 +8,7 @@ var config = {
         app: { name: 'UCCSS' },
         port: 5000,
         db: 'mongodb://127.0.0.1/helpMe-dev',
-        secret: "cayennedlikedhistreats",
-        uploads: './public/uploadedFiles'
+        secret: "cayennedlikedhistreats"
     },
 
     test: {
@@ -17,8 +16,7 @@ var config = {
         app: { name: 'UCCSS' },
         port: 4000,
         db: 'mongodb://127.0.0.1/helpMe-test',
-        secret: "cayennedlikedhistreats",
-        uploads: './public/uploadedFiles'
+        secret: "cayennedlikedhistreats"
     },
 
     production: {
@@ -26,8 +24,7 @@ var config = {
         app: { name: 'UCCSS' },
         port: 80,
         db: 'mongodb://127.0.0.1/helpMe',
-        secret: "cayennedlikedhistreats",
-        uploads: './public/uploadedFiles'
+        secret: "cayennedlikedhistreats"
     }
 };
 
