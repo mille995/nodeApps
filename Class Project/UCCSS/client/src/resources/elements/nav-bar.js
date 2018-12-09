b38d3deaa05281598b05ba50e94b668cbc4486b9
Joyce Miller <mille995@uwm.edu>
2018-12-09 11:51:32 -0600

    nav-bar.js

--- Class Project/UCCSS/client/src/resources/elements/nav-bar.js
+++ Class Project/UCCSS/client/src/resources/elements/nav-bar.js
@@ -45,7 +45,7 @@ export class NavBar {
 
     logout() {
         if (this.userObj) this.auth.logout(this.userObj.email);
-        sessionStorage.removeItem("userObj");
+        sessionStorage.removeItem('user');
         this.isAuthenticated = this.auth.isAuthenticated();
         this.auth.logout();
     }
